a3a4dce1ea4a747c5c193019116170f010913449
mariavalery <valeryleon2107@gmail.com>
2025-07-17 17:48:35 +0530

    modified search tool tip

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -65,10 +65,10 @@ function Navbar() {
         </a>
 
         {/* Search Bar */}
-        <div className="hidden md:flex flex-1 items-center bg-blue-50 px-2 py-1 rounded-md">
+        <div className="hidden md:flex flex-1 items-center bg-blue-50 px-2 py-1 rounded-md" title="Search for Products, Brands and More">
           <div>
             <button>
-              <svg width="24" height="24"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><title>Search Icon</title><path d="M10.5 18C14.6421 18 18 14.6421 18 10.5C18 6.35786 14.6421 3 10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18Z" stroke="#717478" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"></path><path d="M16 16L21 21" stroke="#717478" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+              <svg width="24" height="24"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10.5 18C14.6421 18 18 14.6421 18 10.5C18 6.35786 14.6421 3 10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18Z" stroke="#717478" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"></path><path d="M16 16L21 21" stroke="#717478" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"></path></svg>
             </button>
           </div>
           <input
