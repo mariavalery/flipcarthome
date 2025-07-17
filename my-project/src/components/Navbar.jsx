42fb0aa3c2fab7523ffa8163d3b402280588c5da
mariavalery <valeryleon2107@gmail.com>
2025-07-17 17:45:37 +0530

    Add responsive styling to home nav elements

--- my-project/src/components/Navbar.jsx
+++ my-project/src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import { FaSearch, FaChevronDown } from "react-icons/fa";
 import { useState } from "react";
 import logo from "../assets/flipkartlogo.svg";
+// import searchicon from "..assets/searchicon.svg";
+import shortcuticon from "../assets/shortcuticon.svg";
+import baricon from "../assets/baricon.svg";
 import usericon from "../assets/usericon.svg";
 import arrowupicon from "../assets/arrowupicon.svg";
 import arrowdownicon from "../assets/arrowdownicon.svg";
@@ -28,7 +31,11 @@ const loginmenuItems = [
 ];
 
 const helpmenuItems = [
-  { icon: notificationPreferences, label: "Notification Preferences", href: "/notificationPreferences" },
+  {
+    icon: notificationPreferences,
+    label: "Notification Preferences",
+    href: "/notificationPreferences",
+  },
   { icon: helpcenter, label: "24 x 7 Support Care", href: "/support" },
   { icon: advertise, label: "Advertise", href: "/advertise" },
   { icon: download, label: "Download App", href: "/download" },
@@ -39,18 +46,31 @@ function Navbar() {
   const [isHelpHovered, setIsHelpHovered] = useState(false);
 
   return (
-    <nav className="w-full bg-white px-6 py-2 flex justify-between items-center relative shadow-sm">
-      {/* Logo */}
-      <div>
-        <a href="/" className="flex items-center">
-          <img src={logo} title="Flipkart" alt="flipkart-logo" className="h-10 object-contain" />
-        </a>
+    <nav className="w-full h-16 bg-white px-10 py-2 flex justify-between items-center relative shadow-sm">
+      {/* threeBar */}
+
+      <div className=" md:hidden ">
+        <img src={baricon} />
       </div>
 
-      {/* Search Bar */}
-      <div className="flex-1 items-center mx-6" title="Search for Products, Brands and More">
-        <div className="flex bg-blue-50 px-4 py-2 rounded-md">
-          <FaSearch className="text-gray-500 mr-2" />
+      {/* Logo */}
+      <div className="flex flex-1 items-center mx-2 gap-4">
+        <a href="/" className="shrink-0">
+          <img
+            src={logo}
+            title="Flipkart"
+            alt="flipkart-logo"
+            className="w-40 h-10 object-contain"
+          />
+        </a>
+
+        {/* Search Bar */}
+        <div className="hidden md:flex flex-1 items-center bg-blue-50 px-2 py-1 rounded-md">
+          <div>
+            <button>
+              <svg width="24" height="24"  viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><title>Search Icon</title><path d="M10.5 18C14.6421 18 18 14.6421 18 10.5C18 6.35786 14.6421 3 10.5 3C6.35786 3 3 6.35786 3 10.5C3 14.6421 6.35786 18 10.5 18Z" stroke="#717478" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"></path><path d="M16 16L21 21" stroke="#717478" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+            </button>
+          </div>
           <input
             type="text"
             placeholder="Search for Products, Brands and More"
@@ -60,7 +80,11 @@ function Navbar() {
       </div>
 
       {/* Right Section */}
-      <div className="flex items-center text-gray-800 text-md gap-14">
+      <div className="flex items-center text-gray-800 text-md gap-4">
+        {/* Shortcut*/}
+        <div className=" md:hidden ">
+          <img src={shortcuticon} />
+        </div>
         {/* Login Dropdown */}
         <div
           className="relative"
@@ -68,14 +92,19 @@ function Navbar() {
           onMouseLeave={() => setIsLoginHovered(false)}
         >
           <div
-            className={`inline-flex items-center px-3 py-1 rounded-md cursor-pointer ${
+            className={`inline-flex items-center px-2 py-0 rounded-md cursor-pointer text-md ${
               isLoginHovered ? "bg-blue-600 text-white" : "text-gray-800"
             }`}
-          title="Login"
+            title="Login"
           >
-            <img src={isLoginHovered?userwhiteicon:usericon} alt="user-icon" className="mr-1" />
+            <img
+              src={isLoginHovered ? userwhiteicon : usericon}
+              alt="user-icon"
+              className="mr-1"
+            />
             <span>Login</span>
-            <img src={isLoginHovered?arrowupicon:arrowdownicon}
+            <img
+              src={isLoginHovered ? arrowupicon : arrowdownicon}
               className={`ml-1 transition-transform duration-500 ${
                 isLoginHovered ? "rotate-180" : ""
               }`}
@@ -84,8 +113,11 @@ function Navbar() {
 
           {isLoginHovered && (
             <div className="absolute top-full mt-2 left-0 bg-white shadow-md rounded-md w-56 z-50 text-sm cursor-pointer">
-              <div className="p-3 border-b flex justify-between items-center " title="Sign Up">
-                <span className="text-gray-500 " >New Customer?</span>
+              <div
+                className="p-3 border-b flex justify-between items-center "
+                title="Sign Up"
+              >
+                <span className="text-gray-500 ">New Customer?</span>
                 <a href="/Signup" className="text-blue-600 font-semibold">
                   Sign Up
                 </a>
@@ -110,29 +142,40 @@ function Navbar() {
         </div>
 
         {/* Cart */}
-        <a href="/Cart" title ="Cart" className="flex items-center px-4 py-2 gap-2 cursor-pointer">
+        <a
+          href="/Cart"
+          title="Cart"
+          className="flex items-center px-1 py-0 gap-2 cursor-pointer"
+        >
           <img src={carticon} alt="cart-icon" />
-          <span>Cart</span>
+          <span className="hidden md:flex">Cart</span>
         </a>
 
         {/* Become a Seller */}
-        <a href="/Store" title="Become a Seller" className="flex items-center px-4 py-2 gap-2 cursor-pointer">
+        <a
+          href="/Store"
+          title="Become a Seller"
+          className=" hidden md:flex items-center px-4 py-2 gap-2 cursor-pointer"
+        >
           <img src={storeicon} alt="store-icon" />
           <span>Become a Seller</span>
         </a>
 
         {/* 3-dot Menu */}
         <div
-          className="relative"
+          className="relative hidden md:flex"
           onMouseEnter={() => setIsHelpHovered(true)}
           onMouseLeave={() => setIsHelpHovered(false)}
         >
-          <div className="px-3 py-2 rounded-md hover:bg-gray-50" title="Dropdown with more help links">
+          <div
+            className="px-3 py-2 rounded-md hover:bg-gray-100"
+            title="Dropdown with more help links"
+          >
             <img src={eclipsedoticon} alt="more-icon" />
           </div>
 
           {isHelpHovered && (
-            <div className="absolute top-75 right-0 mt-2 bg-white shadow-lg rounded-lg w-56 z-50 text-sm">
+            <div className="absolute top-full right-0 mt-2 bg-white shadow-lg rounded-lg w-56 z-50 text-sm">
               <ul className="p-2 space-y-2">
                 {helpmenuItems.map((item, index) => (
                   <li key={index}>
