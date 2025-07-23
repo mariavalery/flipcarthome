efed5280d7758150914387281a8881490b5e1768
mariavalery <valeryleon2107@gmail.com>
2025-07-23 10:55:27 +0530

    added responsive to electronics card

--- my-project/src/components/BestOfElectronics.jsx
+++ my-project/src/components/BestOfElectronics.jsx
@@ -94,31 +94,33 @@ function BestOfElectronics() {
           )}
 
           <div className="overflow-y-auto md:overflow-x-auto scroll-smooth snap-x snap-mandatory scrollbar-hide px-4">
-            <div className="flex flex-col md:flex-row md:gap-4">
+            <div className="flex flex-col  md:flex-row md:gap-4">
               {slideShow.map((item) => (
                 <div
                   key={item.id}
-                  className="flex-shrink-0 w-[15.7%] snap-start text-sm"
+                  className="flex-shrink-0  snap-start text-sm  border-b-2 border-gray-300 md:border-none   "
                 >
-                  <div className="flex flex-row md:flex-col items-center md:items-center gap-4 md:gap-3 px-2 py-3 md:p-2 border-b border-gray-100 md:border-none">
+                  <div className="flex items-center  w-full justify-between md:flex-col md:items-center gap-4 px-4 py-3 md:p-2  md:border-none ">
                     {/* Image */}
                     <img
                       src={item.icon}
                       alt={item.label}
-                      className="h-16 w-16 md:h-24 md:w-24 object-contain"
+                      className="h-28 w-28 md:h-24 md:w-24 object-contain shrink-0"
                     />
 
                     {/* Text */}
-                    <div className="flex flex-col justify-center flex-1 md:items-center md:text-center">
-                      <p className="text-sm font-semibold text-black">
+                    <div className="flex-1 min-w-0  md:px-0 md:text-center">
+                      <p className="text-sm font-semibold text-black ">
                         {item.label}
                       </p>
-                      <p className="text-sm text-gray-500">{item.sublabel}</p>
+                      <p className="text-sm text-gray-500 ">
+                        {item.sublabel}
+                      </p>
                     </div>
 
-                    {/* Chevron Icon (Visible only on mobile) */}
-                    <div className="ml-auto md:hidden">
-                      <FaChevronRight className="w-4 h-4 text-gray-500" />
+                    {/* Chevron Icon (Mobile only) */}
+                    <div className="md:hidden shrink-0">
+                      <FaChevronRight className="w-4 h-4 text-gray-400" />
                     </div>
                   </div>
                 </div>
