83a9360e5123754bfcc03aea62813b46f0bec124
mariavalery <valeryleon2107@gmail.com>
2025-07-27 20:17:21 +0530

    add border margin

--- my-project/src/components/FooterSection.jsx
+++ my-project/src/components/FooterSection.jsx
@@ -324,7 +324,7 @@ function FooterSection() {
             </div>
 </div>
               {/* Copy rights content */}
-         <div className="flex px-8 text-white border-b-0 lg:border-t lg:border-[#454d5e] py-5">
+         <div className="flex px-8 text-white border-b-0 lg:border-t lg:border-[#454d5e] pt-5 mt-3">
             <div className="flex gap-8 mx-5">
              {endFooterIcons.map((item)=>(
               <div className="flex gap-2">
